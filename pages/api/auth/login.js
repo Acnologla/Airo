31f19f764da126f1c59ab744a8c0279d8065e18f
chiyoku <chiyokuoni@gmail.com>
2021-02-19 18:51:57 -0300

    Started API and DB relations

---
+++ pages/api/auth/login.js
@@ -0,0 +1,11 @@
+import client from "../models/client.js"
+export default async (req, res) => {
+    if (req.method === "POST"){
+        const {username, password} = req.body
+        const [user] = await client.query("SELECT * EXCEPT password FROM  Users WHERE username = $1 and password = crypt($2, gen_salt('bf', 8))", [username,password])
+        if (!user){
+           return res.status(401).send("Invalid credentials")
+        }
+        res.status(200).json(user)
+    }
+}
