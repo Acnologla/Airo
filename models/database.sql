2ae7456b87700a242c23010687587b8d23618095
unknown <rafaeelsgamer@gmail.com>
2021-02-21 18:10:06 -0300

    Fixed db scripts

--- models/database.sql
+++ models/database.sql
@@ -1,29 +1,25 @@
-CREATE DATABASE Airo;
 CREATE EXTENSION pgcrypto;
 
 CREATE TABLE Users(
-    id SERIAL PRIMARY KEY;
-    username varchar(50) NOT NULL;
-    password TEXT NOT NULL;
-    created TIMESTAMP NOT NULL;
-)
+    id SERIAL PRIMARY KEY,
+    username varchar(50) NOT NULL,
+    password TEXT NOT NULL,
+    created TIMESTAMP NOT NULL
+);
 
 CREATE TABLE Posts(
-    id SERIAL PRIMARY KEY;
-    title VARCHAR(60) NOT NULL;
-    content TEXT NOT NULL;
-    author INT REFERENCES Users (id) NOT NULL;
-    created TIMESTAMP NOT NULL;
-)
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(60) NOT NULL,
+    content TEXT NOT NULL,
+    author INT REFERENCES Users (id) NOT NULL,
+    created TIMESTAMP NOT NULL
+);
 
 CREATE TABLE Comments (
-    id SERIAL PRIMARY KEY;
-    postId INT REFERENCES Posts (id);
-    commentId INT REFERENCES Comments (id);
-    author INT REFERENCES Users (id) NOT NULL;
-    content TEXT NOT NULL;
-    created TIMESTAMP NOT NULL;
+    id SERIAL PRIMARY KEY,
+    postId INT REFERENCES Posts (id),
+    commentId INT REFERENCES Comments (id),
+    author INT REFERENCES Users (id) NOT NULL,
+    content TEXT NOT NULL,
+    created TIMESTAMP NOT NULL
 )
-
-
-
--- models/migrate.js
+++ models/migrate.js
@@ -1,12 +1,19 @@
+const path = require("path")
+require('dotenv').config({
+    path: path.resolve(process.cwd(), '.env.local')
+})
 const fs = require("fs")
-const createDatabaseScript = fs.readFileSync(`${__dirname}/models/database.sql`, "utf-8")
-const client = require("./client.js")
-
-client.query(createDatabaseScript)
-    .then(() => console.log(`Database Created`))
-    .catch(error => {
-        console.error(error)
-        console.error("Failed to create database")
-    })
-
-client.end()
+const createDatabaseScript = fs.readFileSync(`${__dirname}/database.sql`, "utf-8")
+const { Client } = require('pg')
+const client = new Client()
+async function createDb() {
+    try {
+        await client.query(createDatabaseScript)
+        console.log("Database created")
+    } catch (err) {
+        console.log(err)
+        console.log(`Failed to create database\n${err}`)
+    }
+    client.end()
+}
+client.connect().then(createDb)
