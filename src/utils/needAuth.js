7ff84fc0590c76c1015d002ee3df04ec7e8b674b
unknown <rafaeelsgamer@gmail.com>
2021-02-21 18:36:33 -0300

    Fixed needAuth function

--- src/utils/needAuth.js
+++ src/utils/needAuth.js
@@ -1,9 +1,12 @@
 import { verify } from "jsonwebtoken"
 export default (callback) => {
+  return (req, res) => {
     const token = req.headers["authorization"]
-    verify(token, process.env.SECRET, function(err, decoded) {
-        if (err) return res.status(401).send("Invalid token")
-        req.userId = decoded.id;
-        callback(req,res)
-      });
+    verify(token, process.env.SECRET, function (err, decoded) {
+      if (err) return res.status(401).send("Invalid token")
+      req.userId = decoded.id;
+      callback(req, res)
+    });
+  }
+
 }
