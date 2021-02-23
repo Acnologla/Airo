333df15e914734cd1285a78c6b874f7e88358041
unknown <rafaeelsgamer@gmail.com>
2021-02-23 00:38:05 -0300

    Added posts route

---
+++ pages/api/posts.js
@@ -0,0 +1,24 @@
+import client from "../../models/client"
+import needAuth from "../../src/utils/needAuth"
+
+export default async (req, res) => {
+    if (req.method === "POST") {
+        return needAuth(async (req, res) => {
+            const { title, content } = req.body
+            const author = req.userID
+            if (!title || !content) return res.status(400).end()
+            const created = new Date()
+            try{
+                const result = await client.query("INSERT INTO Posts(title,content, author, created) VALUES ($1, $2, $3, $4) RETURNING id", [title, content, author, created])
+                res.json({id: result.rows[0].id})
+            }catch(e){
+                console.log(e)
+                res.status(400).end()
+            }
+        })(req,res)
+    } else if (req.method === "GET") {
+        const limit = req.query.limit || 20
+        const result = await client.query("SELECT * FROM Posts LIMIT $1", [limit])
+        return res.json(result.rows)
+    }
+}
