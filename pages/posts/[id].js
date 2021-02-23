c0e496ea4e9ba33f138b793d4e77b4afe0e017f2
unknown <rafaeelsgamer@gmail.com>
2021-02-23 00:38:19 -0300

    Added posts route

---
+++ pages/posts/[id].js
@@ -0,0 +1,38 @@
+import client from "../../models/client"
+
+export default function Post({ post }) {
+    if (!post || !post.id) return <h1>Not found</h1>
+    return (
+        <section>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+            <p>{post.author.username}</p>
+            <p>{post.created}</p>
+        </section>
+    )
+
+}
+export async function getStaticPaths() {
+ return {
+     paths: [],
+     fallback: true
+ }
+}
+export async function getStaticProps({ params }) {
+    if(isNaN(params.id)){
+        params.id = -1
+    }
+    const result = await client.query("SELECT * FROM Posts where id = $1", [params.id])
+    const [post] = result.rows
+    if (post){
+        const author = await client.query("SELECT id, username FROM Users where id = $1", [result.rows[0].author])
+        post.author = author.rows[0]
+        post.created = `${post.created.getDate()}/${post.created.getMonth() + 1}/${post.created.getFullYear()}`
+    } 
+    return {
+        props: {
+            post: post || {} 
+        },
+        revalidate: 120
+    }
+}
