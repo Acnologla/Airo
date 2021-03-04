251b7c8308cea60a9a133ddb50bbc562a6010339
unknown <rafaeelsgamer@gmail.com>
2021-03-03 22:19:06 -0300

    Added rating sort

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,6 +1,7 @@
 import client from "@/models/client"
 import needAuth from "@/src/utils/needAuth"
 import { rateLimit, loginRateLimit, getRateLimit } from "@/src/lib/rateLimit"
+import { formatDate } from "@/src/utils/utils"
 
 export default async (req, res) => {
     if (req.method === "POST") {
@@ -16,11 +17,30 @@ export default async (req, res) => {
                 console.log(e)
                 res.status(400).end()
             }
-        }))(req,res)
+        }))(req, res)
     } else if (req.method === "GET") {
         return rateLimit(getRateLimit, async (req, res) => {
             const limit = req.query.limit || 20
-            const result = await client.query("SELECT * FROM Posts LIMIT $1", [limit])
+            const sort = req.query.sort
+            let query = "SELECT * FROM Posts"
+            if (sort === "1") {
+                query += ` LEFT JOIN (
+                    SELECT postid, count(*) as likes from Upvote 
+                    where isDislike = false
+                    group by postid
+                ) UPVOTE ON id = postid ORDER BY likes`
+            }
+            query += " LIMIT $1"
+            const result = await client.query(query, [limit])
+            for (const post of result.rows) {
+                post.created = formatDate(post.created)
+                const commentsResult = await client.query("SELECT COUNT(*) FROM Comments where postId = $1", [post.id])
+                post.comments = commentsResult.rows[0].count
+                const upvotesResult = await client.query("SELECT COUNT(*) FILTER (where isDislike is true) AS disLikes, COUNT(*) FILTER (where isDislike is false) AS likes FROM Upvote WHERE postId = $1", [post.id])
+                const [{ likes, dislikes }] = upvotesResult.rows
+                post.likes = likes
+                post.dislikes = dislikes
+            }
             return res.json(result.rows)
         })(req, res)
     }
--- src/components/content.js
+++ src/components/content.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import Card from "./card.js"
 import CreatePost from "./createPost"
+import axios from "axios"
 
 export default function Content({posts}) {
   const [postForm, createPost] = useState(false)
+  const [realPosts, setPosts] = useState(posts || [])
+  const rising = async() => {
+    const {data:newPosts} = await axios.get("/api/posts?sort=1")
+    setPosts(newPosts)
+  }
   return (
     <div className="content-center w-full sm:mt-2 relative top-7 ">
       {postForm ? <CreatePost /> : <></>}
@@ -12,11 +18,11 @@ export default function Content({posts}) {
           <center className="rounded-full">
             <button className="block border-b-2 border-red-500  text-red-500  sm:inline-block sm:mr-6">POPULAR</button>
             <button onClick={() => createPost(!postForm)} className="block text-gray-500  sm:inline-block">NEW</button>
-            <button className="block text-gray-500  sm:inline-block  sm:ml-6">RISING</button>
+            <button onClick={rising} className="block text-gray-500  sm:inline-block  sm:ml-6">RISING</button>
           </center>
         </div>
         <div className="cursor-pointer bg-white border mt-2 m-auto shadow-lg sm:mt-0">
-          {posts.map((el, i) =>
+          {realPosts.map((el, i) =>
             <Card
               key={i}
               post={el}
